da70e5b3fb0f6166dc7ef4808ac627f50f5c98c1
Muhammad I. Patel <muhammadpatel7911@gmail.com>
2018-09-17 10:52:28 +0200

    Done the initial setup of codefile

--- prac_4_PTLMUH012_HRLSUN002.py
+++ prac_4_PTLMUH012_HRLSUN002.py
@@ -17,3 +17,52 @@ import os
 GPIO.setwarnings(False)
 # Set pin numbering as GPIO pin numbering
 GPIO.setmode(GPIO.BCM)
+
+
+# Define global variables
+start = 0
+end = 0
+freqcount = 0
+stopbtncount = 0
+delay = 0.5
+monitor = False
+innerArray = [0]*5
+outerArray = []
+innerStr = []
+
+# Format column header
+col_header = ["Time", "Timer", "Pot", "Temp", "Light"]
+
+
+
+#------------------------------------------------------------------------------------------------------
+# SPI pin definition using BCM pin numbering
+CLK = 11
+MISO = 9
+MOSI = 10
+CS = 8
+
+# SPI pin setup
+GPIO.setup(CLK, GPIO.OUT)
+GPIO.setup(MISO, GPIO.IN)
+GPIO.setup(MOSI, GPIO.OUT)
+GPIO.setup(CS, GPIO.OUT)
+
+# Set up MCP3008 ADC
+mcp = Adafruit_MCP3008.MCP3008(clk = CLK,	miso = MISO,	mosi = MOSI,	cs = CS)
+
+# Define sensor channels
+light_channel = 0
+temp_channel  = 1
+pot_channel = 2
+
+# Pushbutton definition using BCM pin numbering
+RESET = 21
+FREQUENCY = 20
+STOP = 16
+DISPLAY = 6
+
+# Setup pushbuttons as GPIO digital inputs, in PULL-UP mode to avoid false detection (when pressed, pulls connection to ground)
+GPIO.setup([RESET, FREQUENCY, STOP, DISPLAY], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+
